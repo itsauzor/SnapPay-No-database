fd8f2bc52574d4278564e93efbd2ee81c35ce7a2
Marjhonie <157345383+marjhonie-siasol@users.noreply.github.com>
2024-06-22 11:31:27 +0800

    Update IntroFrame.java

--- SnapPayApp/src/snappayapp/IntroFrame.java
+++ SnapPayApp/src/snappayapp/IntroFrame.java
@@ -8,6 +8,7 @@ public class IntroFrame extends javax.swing.JFrame {
                 initComponents();
         }     
 
+        
         @SuppressWarnings("unchecked")
         // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
         private void initComponents() {
